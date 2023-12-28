f23e5a4c5686933c586147b4f262974de718d71f
Scott Phillips <polygonguru@gmail.com>
2023-12-28 11:32:08 -0500

    Update post-build.sh

--- react/post-build.sh
+++ react/post-build.sh
@@ -1,4 +1,4 @@
-cp dist-pocketride/css/pocketrides.css ../pocketride/service/wwwroot/css/pocketride.css
-cp dist-pocketride/js/pocketrides.js ../pocketride/service/wwwroot/js/pocketride.js
-cp dist-pearbnb/css/pearbnb.css ../pearbnb/service/wwwroot/css/pearbnb.css
-cp dist-pearbnb/js/pearbnb.js ../pearbnb/service/wwwroot/js/pearbnb.js
+cp dist-pocketride/css/pocketrides.css ../pocketride/service/wwwroot/css/connect.css
+cp dist-pocketride/js/pocketrides.js ../pocketride/service/wwwroot/js/connect.js
+cp dist-pearbnb/css/pearbnb.css ../pearbnb/service/wwwroot/css/connect.css
+cp dist-pearbnb/js/pearbnb.js ../pearbnb/service/wwwroot/js/connect.js
