dd539c8b478751f08eb3964f570fc808e35a7ef2
Scott Phillips <polygonguru@gmail.com>
2023-12-29 14:17:20 -0500

    fix: `prettier`

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -33,10 +33,8 @@ export default defineConfig({
     rollupOptions: {
       output: {
         assetFileNames: (assetInfo) => {
-          if (assetInfo.name === "index.css")
-            return `css/connect.css`;
-          if (assetInfo.name === "assets/index.js")
-            return `connect.js`;
+          if (assetInfo.name === "index.css") return `css/connect.css`;
+          if (assetInfo.name === "assets/index.js") return `connect.js`;
           return assetInfo.name ?? "";
         },
         entryFileNames: `js/${process.env.RELYING_PARTY}.js`,
